63ab3d847f52b2ff121386c783e5abd4133db32c
mtm <monotone.the.musical@gmail.com>
2022-12-06 17:37:43 +1100

    wip

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from setuptools import setup
 from setuptools.command.install import install
 
 setup(
+  name='filever',
+  url='https://github.com/monotone-the-musical/filever',
+  author='mtm',
+  author_email='monotone.the.musical@gmail.com',
   scripts=['files/fver.py', 'files/fver'],
 )
